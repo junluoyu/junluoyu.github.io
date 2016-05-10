657ef44c4b9f6f6a10049e1457176a4c75438f11
junluoyu <850564082@qq.com>
2016-05-10 18:04:43 +0800

    personal page

---
+++ js/image.js
@@ -0,0 +1,75 @@
+// JavaScript Document
+$(document).ready(function(){
+  $("img#1").click(function(){
+    var img1=$("img#1");
+    img1.animate({height:'400px',width:'678px'},"slow");
+  });
+  $("img#2").click(function(){
+    var img2=$("img#2");
+    img2.animate({height:'400px',width:'678px'},"slow");
+  });
+   $("img#3").click(function(){
+    var img3=$("img#3");
+    img3.animate({height:'400px',width:'678px'},"slow");
+  });
+   $("img#4").click(function(){
+    var img4=$("img#4");
+    img4.animate({height:'400px',width:'678px'},"slow");
+  });
+   $("img#5").click(function(){
+    var img5=$("img#5");
+    img5.animate({height:'400px',width:'678px'},"slow");
+  });
+   $("img#6").click(function(){
+    var img6=$("img#6");
+    img6.animate({height:'400px',width:'678px'},"slow");
+  });
+     $("img#7").click(function(){
+    var img6=$("img#7");
+    img6.animate({height:'400px',width:'678px'},"slow");
+  });
+     $("img#8").click(function(){
+    var img6=$("img#8");
+    img6.animate({height:'400px',width:'678px'},"slow");
+  });
+     $("img#9").click(function(){
+    var img6=$("img#9");
+    img6.animate({height:'400px',width:'678px'},"slow");
+  });
+    $("a#a").click(function(){
+    var img=$("img#1");
+    img.animate({height:'150px',width:'200px'},"slow");
+  });  
+    $("a#a").click(function(){
+    var img=$("img#2");
+    img.animate({height:'150px',width:'200px'},"slow");
+  }); 
+      $("a#a").click(function(){
+    var img=$("img#3");
+    img.animate({height:'150px',width:'200px'},"slow");
+  });  
+    $("a#a").click(function(){
+    var img=$("img#4");
+    img.animate({height:'150px',width:'200px'},"slow");
+  });
+      $("a#a").click(function(){
+    var img=$("img#5");
+    img.animate({height:'150px',width:'200px'},"slow");
+  });  
+    $("a#a").click(function(){
+    var img=$("img#6");
+    img.animate({height:'150px',width:'200px'},"slow");
+  });
+      $("a#a").click(function(){
+    var img=$("img#7");
+    img.animate({height:'150px',width:'200px'},"slow");
+  });
+      $("a#a").click(function(){
+    var img=$("img#8");
+    img.animate({height:'150px',width:'200px'},"slow");
+  });
+      $("a#a").click(function(){
+    var img=$("img#9");
+    img.animate({height:'150px',width:'200px'},"slow");
+  });
+});
